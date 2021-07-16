ea959bab4eea0a71dfed9c9f65fad6c7a36f9d47
Myck William dos Santos <myckwilliam@gmail.com>
2021-07-16 17:04:50 -0300

    cleanup starter stuff

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,11 @@ import * as React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const NotFoundPage = () => (
+const IndexPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404</h1>
-    <p>Nada aqui...</p>
+    <Seo title="Home" />
+    <h1>Home</h1>
   </Layout>
 )
 
-export default NotFoundPage
+export default IndexPage
